103dd520f2e6a43b4002489337f701b16d1d842e
root <artyom.fedotov93@gmail.com>
2020-04-21 15:59:30 +0300

    "add solution for 1-module 1-task"

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a == 'number' && typeof b == 'number'){
+    return a+b
+  }
+  throw new TypeError('these arguments are not a numbers!');
 }
+
+
 
 module.exports = sum;
